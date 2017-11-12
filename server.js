e7ec7c78239a66c03c3b8bcfc5654eb931611c36
dermio <thisdjay@gmail.com>
2017-11-12 12:35:55 -0800

    Required morgan, exported Express app.

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
+app.use(morgan("common"));
 app.use(express.static("public"));
+
 app.listen(process.env.PORT || 8080);
+
+module.exports = {app};
