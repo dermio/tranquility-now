91b136e612a05f3b126756318ee763e659bc1998
dermio <thisdjay@gmail.com>
2017-11-13 17:40:46 -0800

    Required body-parser and mongoose.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+mongoose.Promise = global.Promise;
 
 app.listen(process.env.PORT || 8080);
 
