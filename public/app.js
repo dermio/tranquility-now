26dbf2b9e4e6afea1e5c1dbf31a9decac52a758d
dermio <thisdjay@gmail.com>
2017-11-16 17:27:27 -0800

    Edited AJAX GET request for button click.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,19 @@
 function handler() {
-  let myUrl = "/";
-  
-  $.ajax({
-    type: "GET",
-    url: myUrl,
-    dataType: "json"
-  });
+  let myUrl = "http://localhost:8080";
 
   $("button").on("click", function (event) {
     event.preventDefault();
     console.log("button clicked!");
+
+    $.ajax({
+      method: "GET",
+      url: myUrl + "/all-stressors",
+      dataType: "json",
+      success: function (data) {
+        console.log("[[CLIENT SIDE]]", data);
+      }
+    });
+
   });
 }
 
