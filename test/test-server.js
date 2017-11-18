95309ac4ec676d6d4d53eb3bfc235f74ad4b450d
dermio <thisdjay@gmail.com>
2017-11-18 14:00:58 -0800

    Required runServer and closeServer, and the Stressor model.

--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,8 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 
-const {app} = require("../server");
+const {app, runServer, closeServer} = require("../server");
+const {Stressor} = require("../models");
 
 const should = chai.should();
 chai.use(chaiHttp);
