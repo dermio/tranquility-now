edbb242797f10657cf511c431e5a27b1fe1f3003
dermio <thisdjay@gmail.com>
2017-11-18 17:46:18 -0800

    Edited test database url.

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ exports.PORT = process.env.PORT || 8080;
 
 // need TEST_DATABASE_URL for integration testing
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                            "mongodb://localhost/stress-app";
+                            "mongodb://localhost/test-stress-app";
