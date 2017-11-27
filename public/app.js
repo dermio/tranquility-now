3b01e8088cca03106f0534ed1577f9cbd7d320e5
dermio <thisdjay@gmail.com>
2017-11-27 13:55:05 -0800

    Added displaySearchResults function and comments.

--- public/app.js
+++ public/app.js
@@ -2,7 +2,19 @@ const MY_DATA = {}; // data from database stored here
 
 
 function displaySearchResults() {
+  let htmlString = "";
+  let stressorsData = MY_DATA.data;
 
+  for (let i = 0; i < stressorsData.length; i++) {
+    console.log(i);
+
+    htmlString +=
+      `<div class="js-single-result">
+        <p>${stressorsData[i].activity}</p>
+      </div>`;
+  }
+
+  $(".js-results-stressors").html(htmlString);
 }
 
 function handler() {
@@ -41,5 +53,26 @@ Note, these steps occur after the user has logged into their Dashboard
 
 2. press button or link, get single unique stressor and de-stressing activity for user. GET requesty by id
 
+2b. Also POST data with a form, then display
+
 3. render the results
+
+9. Do this later, will make modular later
+
+Eventually move HTTP routes into a separate router module
+use express.Router
+
+example:
+var express = require('express')
+var router = express.Router()
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+U1, Lesson 4, Section 6, First Glitch recipesRouter.js and 
+shoppingListRouter.js
+
 */
